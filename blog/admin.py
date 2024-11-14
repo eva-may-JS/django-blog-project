7d30ad2d1e099f387a5261d071486bdbd29c61e4
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-11-14 21:19:54 +0000

    Build Post model and update database

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# This will allow to create, update and delete blog posts from the admin panel
+admin.site.register(Post)
