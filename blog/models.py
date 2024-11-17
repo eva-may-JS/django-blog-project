1d764ed793313ea57de65d15f833ffffda2496ea
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-11-17 14:34:48 +0000

    Add excerpt field to Post, debugto True

--- blog/models.py
+++ blog/models.py
@@ -15,3 +15,4 @@ class Post(models.Model):
     # The auto_now_add=True means the default created time is the time of post entry.
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
