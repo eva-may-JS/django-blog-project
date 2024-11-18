33aeb60ba975a47c21cb538117d8bedaed4f6d16
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-11-18 15:43:44 +0000

    Import and register Comment on admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 
 # This will allow to create, update and delete blog posts from the admin panel
 admin.site.register(Post)
+admin.site.register(Comment)
