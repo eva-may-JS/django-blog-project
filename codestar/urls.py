1f1f99b284ebc5d7a8a1869c1eedbf7a8d6e474f
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-11-18 18:08:53 +0000

    Add urls to blog app and post_list.html to blog/templates/blog

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as hello_views
+
 
 urlpatterns = [
-    path('hello/', hello_views.hello, name='hello'),
+    path("", include("blog.urls"), name='blog-urls'),
     path('admin/', admin.site.urls),
 ]
