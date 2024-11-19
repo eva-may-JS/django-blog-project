904ee59ded830df44fa38a65152c3245a67ce888
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-11-19 16:41:31 +0000

    Changed db due to unsolvable error

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+
 
 # Register your models here.
 
-# This will allow to create, update and delete blog posts from the admin panel
-admin.site.register(Post)
+# This will allow to create, update and delete blog posts from the admin panel. Commented out as we have now 
+# registered it above with the decorator @admin.register(Post)
+# admin.site.register(Post)
 admin.site.register(Comment)
