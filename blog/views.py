9315feeae288fc9eec841dc7ce2649b39f43da46
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-11-24 14:20:07 +0000

    Add comment form with confirmation message

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import Post
+from .forms import CommentForm
 
 
 # Create your views here.
@@ -31,6 +33,22 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
 
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
+
+
+    comment_form = CommentForm()
+
     # render combines the chosen template with the dictionary object specified, in this case {"post": post},
     # so the information in the dictionary can be inserted into the template
     return render(
@@ -42,5 +60,6 @@ def post_detail(request, slug):
         {"post": post,
         "comments": comments,
         "comment_count": comment_count,
+        "comment_form": comment_form,
         },
     )
