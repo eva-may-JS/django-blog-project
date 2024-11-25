43457d7eb3174fa6d9db6b686fdcc1fc02c57a0e
Eva Juez <xv4w04bu@students.codeinstitute.net>
2024-11-25 14:31:07 +0000

    added comment CRUD

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     # slug after the colon gets its value from the URL path to post_detail, in index.html
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>',
+         views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),
 ]
